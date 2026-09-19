2b0276b1a188b49210b1348d303c6a37e20c5ee6
Bruno Palacios <brunopalacios2006@gmail.com>
2026-09-18 21:14:17 -0500

    Add main.cpp with menu connecting Resource, Reservation, Queue, and History modules

---
+++ src/main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <limits>
+#include "Resource.h"
+#include "ResourceManager.h"
+#include "Reservation.h"
+#include "Reservationlist.h"
+#include "WaitingQueue.h"
+#include "CancellationHistory.h"
+using namespace std;
+    int main(){
+        int choice, sortChoice;
+        ResourceManager resourceManager;
+        ReservationList reservationList;
+        WaitingQueue waitingQueue;
+        CancellationHistory cancellationHistory;
+resourceManager.loadFromFile("data/resources.txt"); 
+        do{
+            cout << "===== Campus Resource Reservation System =====" << endl;
+            cout << "1. View Resources" << endl;
+            cout << "2. Create Reservation" << endl;
+            cout << "3. Cancel Reservation" << endl;
+            cout << "4. View Waiting List" << endl;
+            cout << "5. Undo Cancellation" << endl;
+            cout << "6. Search Reservations" << endl;
+            cout << "7. Sort Resources" << endl;
+            cout << "8. Generate Report" << endl;
+            cout << "9. Exit" << endl;
+
+            cout << endl << "Enter Choice: ";
+            cin >> choice;
+            if(cin.fail()){
+                cout << "That is not a valid number." << endl;
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                continue;
+            }
+            switch (choice){
+                case 1:
+                    cout << "Displaying available resources..." << endl;
+                    resourceManager.displayAll();
+                    break;
+                case 2:
+                    cout << "Opening reservation form..." << endl;
+                    break;
+                case 3:
+                    cout << "Canceling reservation..." << endl;
+
+                    break;
+                case 4:
+                    cout << "Loading waiting list..." << endl;
+                    waitingQueue.displayWaitingList();
+                    break;
+                case 5:
+                    cout << "Undoing last cancellation..." << endl;
+                    break;
+                case 6:
+                    cout << "Searching for reservations..." << endl;
+                    reservationList.displayReservations();
+                    break;
+                case 7:
+                    cout << "Please make a selection." << endl; 
+                    cout << "1. Sort by Name  |  2. Sort by Type" << endl;
+                    cin >> sortChoice;
+                    if(sortChoice == 1){
+                        cout << "Sorting resources by NAME..." << endl; 
+                        resourceManager.sortByName();
+                    }
+                    else if(sortChoice == 2){
+                        cout << "Sorting resources by TYPE..." << endl;
+                        resourceManager.sortByType();
+                    }
+                    resourceManager.displayAll();
+                    break;
+                case 8:
+                    cout << "Generating usage report..." << endl;
+                    cout << "This feature is not implemented in Milestone 1." << endl;
+                    break;
+                case 9:
+                    cout << "Exiting system." << endl;
+                    break;
+                default:
+                    cout << "Invalid Choice! Please select between 1 and 9." << endl;
+            }
+        }while (choice != 9);
+        return 0;
+    }
+    
